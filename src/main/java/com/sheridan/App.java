af2180c8355b453e29bdcc2550c4c9bee8a994be
Harshdeep Singh <sin14375@sheridancollege.ca>
2025-03-02 02:36:48 -0500

    Updated App.java with new changes

--- src/main/java/com/sheridan/App.java
+++ src/main/java/com/sheridan/App.java
@@ -5,5 +5,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "This is Java Project named DemoHaarshdeep for InClass3" );
+        System.out.println( "This is to be added for testing git-actions" );
     }
 }
