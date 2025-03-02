83df3223aedff7bf9d0ff983309e4ad498729236
Harshdeep Singh <sin14375@sheridancollege.ca>
2025-03-01 19:07:40 -0500

    Initial Commit

---
+++ src/main/java/com/sheridan/App.java
@@ -0,0 +1,9 @@
+package com.sheridan;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is Java Project named DemoHaarshdeep for InClass3" );
+    }
+}
